2d1fb71d4d838f73382066995c8b9e0e99128677
Byron <byronogis@outlook.com>
2023-02-19 04:04:26 +0800

    feat(vv3tp): auto import api and vue component

--- packages/vite-vue3-ts-practice/vite.config.ts
+++ packages/vite-vue3-ts-practice/vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
+import VueComponents from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Pages(),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+      dts: 'src/types/auto-imports.d.ts',
+      dirs: [],
+    }),
+    VueComponents({
+      dts: 'src/types/vue-components.d.ts',
+      dirs: ['src/components'],
+    }),
   ],
 })
