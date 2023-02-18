67d9f81cb425cbe6c33c4aebb96283206815396c
liamrad <liamradhere@outlook.com>
2023-02-19 06:31:25 +0800

    feat(vv3tp): add ep ui and config its auto import

---
+++ packages/vite-vue3-ts-practice/src/pages/ep/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const open = () => {
+  ElMessage('this is a message.')
+}
+</script>
+
+<template>
+  <el-button :plain="true" @click="open">
+    Show message
+  </el-button>
+</template>
--- packages/vite-vue3-ts-practice/src/pages/index.vue
+++ packages/vite-vue3-ts-practice/src/pages/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>pages/index</div>
+  <BaseSideRoutes />
 </template>
