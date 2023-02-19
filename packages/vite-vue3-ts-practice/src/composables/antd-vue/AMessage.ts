9d6ae9fd0fdb68ea6e5cd33421d7c17ec99ea3d2
Byron <byronogis@outlook.com>
2023-02-19 09:34:31 +0800

    feat(vv3tp): add antd-vue ui/icon and config its auto import

---
+++ packages/vite-vue3-ts-practice/src/composables/antd-vue/AMessage.ts
@@ -0,0 +1,4 @@
+import { message as AMessage } from 'ant-design-vue'
+import 'ant-design-vue/es/message/style/css' // vite只能用 ant-design-vue/es 而非 ant-design-vue/lib
+
+export { AMessage }
---
+++ packages/vite-vue3-ts-practice/src/pages/antd/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const open = () => {
+  AMessage.info('this is a message.')
+}
+</script>
+
+<template>
+  <a-button type="primary" @click="open">
+    Show message
+  </a-button>
+</template>
