8eeca0c653f06971451ff6e52c6207dbb2ee1467
liamrad <liamradhere@outlook.com>
2023-02-25 15:26:46 +0800

    chore(vv3tp): range style

--- packages/vite-vue3-ts-practice/src/components/DatePicker/DatePickerHalfQuarterYearPanelWrapper.vue
+++ packages/vite-vue3-ts-practice/src/components/DatePicker/DatePickerHalfQuarterYearPanelWrapper.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
+const props = defineProps<{
+  isRange?: boolean
+}>()
+
 const emits = defineEmits(['update:popoverVisible'])
+
+const isRange = toRef(props, 'isRange')
 </script>
 
 <template>
   <div
     tabindex="0"
-    class="el-picker-panel el-date-picker"
+    class="el-picker-panel"
+    :class="!isRange ? 'el-date-picker' : 'el-date-range-picker'"
     @focus="emits('update:popoverVisible', true)"
     @blur="emits('update:popoverVisible', false)"
   >
     <div class="el-picker-panel__body-wrapper">
       <div class="el-picker-panel__body">
         <!-- content -->
-        <slot />
+        <template v-if="isRange">
+          <slot name="range-left" />
+
+          <slot name="range-right" />
+        </template>
+
+        <template v-else>
+          <slot name="default" />
+        </template>
       </div>
     </div>
   </div>
