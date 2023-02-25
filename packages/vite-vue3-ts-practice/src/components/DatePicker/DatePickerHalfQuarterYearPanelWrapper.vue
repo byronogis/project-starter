fb47b0ad80dcf9aaf04488c2be575ea438a667a1
Byron <byronogis@outlook.com>
2023-02-25 13:09:50 +0800

    chore(vv3tp): datepicker panel wrapper split

---
+++ packages/vite-vue3-ts-practice/src/components/DatePicker/DatePickerHalfQuarterYearPanelWrapper.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="el-picker-panel el-date-picker">
+    <div class="el-picker-panel__body-wrapper">
+      <div class="el-picker-panel__body">
+        <!-- content -->
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
