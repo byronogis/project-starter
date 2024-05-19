24d331482c46e164cf923d0b367ed6e490c8ff69
byronogis <byronogis@outlook.com>
2024-05-19 23:07:01 +0800

    feat: custom icon collection on unocss

---
+++ packages/vue-ts/src/pages/example/unocss.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page-example-unocss">
+    <h1>Unocss</h1>
+    <div class="bg-red:700 p-4 text-white">
+      bg-red-500 p-4 text-white
+    </div>
+
+    <div class="bg-gray:400">
+      <i class="i-mdi:ab-testing" />
+      <i class="i-mdi:account-supervisor-circle" />
+
+      <div class="i-vscode-icons:file-type-light-pnpm" />
+      <div class="i-vscode-icons:file-type-light-pnpm?mask text-red-300" />
+
+      <div class="i-custom:shopping-cart" />
+      <div class="i-custom:assistant" />
+      <div class="i-custom:location cursor-pointer" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="postcss"></style>
--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,6 @@
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
+import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
 import {
   defineConfig,
   presetIcons,
@@ -11,6 +14,19 @@ export default defineConfig({
     presetUno(),
     presetIcons({
       scale: 1.2,
+      autoInstall: true,
+      collections: {
+        custom: FileSystemIconLoader(
+          // place your custom icons in `src/assets/icons/custom`,
+          // eg: `packages/vue-ts/src/assets/icons/custom` for `./packages/vue-ts` (cwd(): /path/to/packages/vue-ts)
+          `${resolve(cwd(), 'src/assets/icons/custom')}`,
+          svg => svg.replace(/#fff/, 'currentColor'),
+        ),
+      },
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
     }),
 
   ],
