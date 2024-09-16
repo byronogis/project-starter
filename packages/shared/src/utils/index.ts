4a2eee6cf1b9f3622ec574d990ee8bd3b830397e
byronogis <byronogis@outlook.com>
2024-09-16 19:52:56 +0800

    feat(packages/shared): add timer

--- packages/shared/src/utils/index.ts
+++ packages/shared/src/utils/index.ts
@@ -2,3 +2,22 @@ export { HTTP } from './http'
 export { logger } from './logger'
 export { date } from './date'
 export { _ } from './_'
+export { timer } from './timer'
+
+/**
+ * 安全的 JSON 解析 \
+ * eg: `const res = safeJSONParse(str).value ?? {} // 默认值`
+ */
+export function safeJSONParse(text: string, reviver?: (this: any, key: string, value: any) => any) {
+  try {
+    return {
+      value: JSON.parse(text, reviver),
+    }
+  }
+  catch (error) {
+    // console.error(error)
+    return {
+      error,
+    }
+  }
+}
---
+++ packages/shared/src/utils/timer.ts
@@ -0,0 +1,13 @@
+import {
+  clearInterval,
+  clearTimeout,
+  setInterval,
+  setTimeout,
+} from 'worker-timers'
+
+export const timer = {
+  setTimeout,
+  setInterval,
+  clearTimeout,
+  clearInterval,
+}
