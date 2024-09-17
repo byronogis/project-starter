13e2c848eacf7c7a1a121ba452b3d64376157e7b
byronogis <byronogis@outlook.com>
2024-09-17 13:59:07 +0800

    feat(apps/nuxt): add useAppToast powered on primevue toast

--- apps/nuxt/app/pages/sakai.vue
+++ apps/nuxt/app/pages/sakai.vue
@@ -2,12 +2,15 @@
 definePageMeta({
   layout: 'sakai',
 })
+
+const appToast = useAppToast()
 </script>
 
 <template>
   <div id="page-sakai">
     <h4>Sakai Page</h4>
     <PrimeInputText />
+    <PrimeButton label="fine" @click="appToast.toastSuccess('fine')" />
   </div>
 </template>
 
