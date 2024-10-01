b857cbc2fd6bffb668544db983b8cda5af2e77d9
byronogis <byronogis@outlook.com>
2024-10-01 15:52:15 +0800

    feat(packages/nuxt-layer-sakai): add extra options to useSakaiToast

--- packages/nuxt-layer-sakai/composables/useSakaiToast.ts
+++ packages/nuxt-layer-sakai/composables/useSakaiToast.ts
@@ -1,31 +1,37 @@
+import type { ToastMessageOptions } from 'primevue/toast'
+
 export function useSakaiToast() {
   const toast = useToast()
 
   return {
     toast,
-    toastSuccess: (message: string) => toast.add({
+    toastSuccess: (message: string, extraOptions: ToastMessageOptions = {}) => toast.add({
       severity: 'success',
       summary: 'Successful',
       detail: message,
       life: 3000,
+      ...extraOptions,
     }),
-    toastError: (message: string) => toast.add({
+    toastError: (message: string, extraOptions: ToastMessageOptions = {}) => toast.add({
       severity: 'error',
       summary: 'Error',
       detail: message,
       life: 3000,
+      ...extraOptions,
     }),
-    toastWarn: (message: string) => toast.add({
+    toastWarn: (message: string, extraOptions: ToastMessageOptions = {}) => toast.add({
       severity: 'warn',
       summary: 'Warn',
       detail: message,
       life: 3000,
+      ...extraOptions,
     }),
-    toastInfo: (message: string) => toast.add({
+    toastInfo: (message: string, extraOptions: ToastMessageOptions = {}) => toast.add({
       severity: 'info',
       summary: 'Info',
       detail: message,
       life: 3000,
+      ...extraOptions,
     }),
   }
 }
