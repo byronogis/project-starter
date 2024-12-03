dcfe3776cd4f8286fa2522af3b935743acf2c707
byronogis <byronogis@outlook.com>
2024-12-03 20:55:14 +0800

    chore(apps/nuxt/api): update example

--- apps/nuxt/app/api/index.ts
+++ apps/nuxt/app/api/index.ts
@@ -1,3 +1,4 @@
+import type { RequestConfig } from '@project-starter/shared/utils/http'
 import { HTTP } from '@project-starter/shared/utils/http'
 
 /**
@@ -6,7 +7,7 @@ import { HTTP } from '@project-starter/shared/utils/http'
 export const exampleHttp = new HTTP({
   baseURL: '/api',
 })
-export function exampleAPI(data: ExampleData, extraConfig: any = {}) {
+export function exampleAPI(data: ExampleData, extraConfig: RequestConfig = {}) {
   return exampleHttp.request<ExampleData, ExampleResult>({
     url: '/example',
     method: 'GET',
