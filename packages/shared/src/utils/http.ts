b392aa44cb4ecd98a9819bea63786d289e46a66d
byronogis <byronogis@outlook.com>
2024-12-03 20:54:19 +0800

    chore(packages/shared/utils): pure HTTP

--- packages/shared/src/utils/http.ts
+++ packages/shared/src/utils/http.ts
@@ -19,42 +19,9 @@ export class HTTP {
     : [axios.defaults.transformResponse]
 
   instance: AxiosInstance
-  pendingMap: Map<string, PendingRequest> = new Map()
 
   constructor(config: CreateHTTPConfig = {}) {
     this.instance = axios.create(config)
-
-    const {
-      _isAborted = false,
-      _isDeDuplicate = false,
-      _isStorePending = false,
-    } = config
-
-    // 请求拦截器
-    const requestInterceptors = [
-      _isDeDuplicate && this.#deDuplication,
-      _isStorePending && this.#addPendingRequest,
-      _isStorePending && _isAborted && this.#addAbortController,
-    ].reverse() // NOTE: 由于请求拦截器是按添加顺序的倒序执行, 所以这里反转以使执行顺序和上面数组的定义顺序一致
-    requestInterceptors.forEach((interceptor) => {
-      if (!interceptor) {
-        return
-      }
-      // TODO fix type AxiosResponse InternalRequestConfig
-      this.instance.interceptors.request.use(interceptor.bind(this) as any)
-    })
-
-    // 响应拦截器
-    const responseInterceptors = [
-      _isStorePending && this.#removePendingRequest,
-    ]
-    responseInterceptors.forEach((interceptor) => {
-      if (!interceptor) {
-        return
-      }
-      // TODO fix type AxiosResponse InternalRequestConfig
-      this.instance.interceptors.response.use(interceptor.bind(this) as any)
-    })
   }
 
   /**
@@ -63,107 +30,6 @@ export class HTTP {
   request<T = any, R = any>(config: RequestConfig<T>): Promise<Response<R, T>> {
     return this.instance.request(config)
   }
-
-  /**
-   * 取消请求
-   */
-  cancel(key: string) {
-    const { cancel } = this.pendingMap.get(key) ?? {}
-    cancel?.()
-    this.pendingMap.delete(key)
-  }
-
-  /**
-   * 取消所有请求
-   */
-  cancelAll() {
-    this.pendingMap.forEach(({ cancel }) => {
-      cancel?.()
-    })
-    this.pendingMap.clear()
-  }
-
-  /**
-   * 存储请求
-   */
-  #addPendingRequest(requestConfig: InternalRequestConfig): Promise<InternalRequestConfig> {
-    const key = this.#getKey(requestConfig)
-    this.pendingMap.set(key, {
-      key,
-      config: requestConfig,
-    })
-
-    requestConfig._key ??= key
-    /**
-     * 自定义 validateStatus \
-     * NOTE: 防止因失败返回而无法清理 pendingMap 中存储的数据 \
-     * 1. 首先使用 axios 默认的 validateStatus 判断 \
-     * 2. 如果失败则清理 pendingMap 中的数据
-     */
-    requestConfig.validateStatus = (status) => {
-      const isSuccess = status >= 200 && status < 300 // axios validateStatus default
-
-      if (!isSuccess) {
-        this.#removePendingRequest({ config: requestConfig } as Response)
-      }
-
-      return isSuccess
-    }
-
-    return Promise.resolve(requestConfig)
-  }
-
-  /**
-   * 移除存储的请求
-   */
-  #removePendingRequest(response: Response): Promise<Response> {
-    const key = response.config._key
-    this.pendingMap.delete(key)
-    return Promise.resolve(response)
-  }
-
-  /**
-   * 添加取消请求控制器
-   */
-  #addAbortController(requestConfig: InternalRequestConfig): Promise<InternalRequestConfig> {
-    const controller = new AbortController()
-    requestConfig.signal = controller.signal
-
-    const key = requestConfig._key
-    this.pendingMap.get(key)!.cancel = () => {
-      controller.abort()
-      this.pendingMap.delete(key)
-    }
-
-    return Promise.resolve(requestConfig)
-  }
-
-  /**
-   * 阻止重复请求
-   */
-  #deDuplication(requestConfig: InternalRequestConfig): Promise<InternalRequestConfig> {
-    const key = this.#getKey(requestConfig)
-    if (this.pendingMap.has(key)) {
-      return Promise.reject(new Error('Duplicate request'))
-    }
-    return Promise.resolve(requestConfig)
-  }
-
-  /**
-   * 获取请求的请求标识
-   */
-  #getKey(requestConfig: InternalRequestConfig): RequestKey {
-    return requestConfig._key ?? this.#generateKey(requestConfig)
-  }
-
-  /**
-   * 默认的请求标识生成 \
-   */
-  #generateKey(requestConfig: InternalRequestConfig): RequestKey {
-    const method = (requestConfig.method ?? 'get').toUpperCase()
-    const url = requestConfig.url ?? ''
-    return `${method}:${url}`
-  }
 }
 
 /**
@@ -171,36 +37,6 @@ export class HTTP {
  */
 interface CreateHTTPConfig extends CreateAxiosDefaults {
   // ...
-  /**
-   * 是否启用存储请求
-   * @default false
-   */
-  _isStorePending?: boolean
-  /**
-   * 是否启用取消请求
-   * @default false
-   */
-  _isAborted?: boolean
-  /**
-   * 是否启用阻止重复请求
-   * @default false
-   */
-  _isDeDuplicate?: boolean
-}
-
-interface PendingRequest {
-  /**
-   * 请求标识
-   */
-  key: RequestKey
-  /**
-   * 请求配置
-   */
-  config: InternalRequestConfig
-  /**
-   * 取消请求
-   */
-  cancel?: () => void
 }
 
 /**
@@ -225,17 +61,5 @@ type Response<R = any, T = any> = AxiosResponse<R, T> & { config: InternalReques
  * 额外的请求配置
  */
 export interface ExtraRequestConfig {
-  /**
-   * 请求标识 \
-   * 1. 用于存储请求 \
-   * 2. 用于取消请求 \
-   * @default `${method}:${url}`
-   * @see #generateKey
-   */
-  _key?: RequestKey
+  // ...
 }
-
-/**
- * 请求标识的类型
- */
-type RequestKey = string
