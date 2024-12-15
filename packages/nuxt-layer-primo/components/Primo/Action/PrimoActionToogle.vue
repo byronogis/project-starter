a31f074507831ec5255e57605cc4999ae19c0519
byronogis <byronogis@outlook.com>
2024-12-15 16:31:18 +0800

    refactor(nuxt-layer-primo): site title

---
+++ packages/nuxt-layer-primo/components/Primo/Action/PrimoActionToogle.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const primoStore = inject(PrimoStoreInjectionKey)!
+</script>
+
+<template>
+  <div class="component-primo-action-toggle">
+    <PrimeButton
+      class="component-primo-action-toggle-btn"
+      aria-label="Toggle"
+      variant="text"
+      rounded
+      severity="secondary"
+      @click="primoStore.onMenuToggle"
+    >
+      <template #icon>
+        <NuxtIcon name="i-prime:bars" class="size-6" />
+      </template>
+    </PrimeButton>
+  </div>
+</template>
+
+<style scoped lang="postcss"></style>
---
+++ packages/nuxt-layer-primo/components/Primo/PrimoTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const primoStore = inject(PrimoStoreInjectionKey)!
+</script>
+
+<template>
+  <NuxtLink to="/" class="component-primo-title flex items-center gap-2">
+    <NuxtIcon name="i-primo:logo" class="size-12 color-[var(--primary-color)]" />
+
+    <span class="text-6 font-500">{{ primoStore.title }}</span>
+  </NuxtLink>
+</template>
+
+<style scoped lang="postcss"></style>
