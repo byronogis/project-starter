af40cc090d4e6bac16ff5b27e0ec617d3fed8470
byronogis <byronogis@outlook.com>
2024-12-16 19:02:41 +0800

    chore(nuxt-layer-primo): remove sidebar padding under lg size

--- packages/nuxt-layer-primo/components/Primo/PrimoSidebar.vue
+++ packages/nuxt-layer-primo/components/Primo/PrimoSidebar.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div class="layout-sidebar component-primo-sidebar grid grid-rows-[auto_1fr_auto] gap-2">
+  <div class="layout-sidebar component-primo-sidebar grid grid-rows-[auto_1fr_auto] gap-2 lg:p-0">
     <PrimoTitle class="!lt-lg:hidden" />
 
     <PrimeScrollPanel class="min-h-0">
