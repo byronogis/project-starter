76843a6fd6ae36beb4da9f5c76b01bf2f6f68f76
byronogis <byronogis@outlook.com>
2025-01-07 12:32:31 +0800

    chore(nuxt-layer-primo/playground): uodate chart color

--- packages/nuxt-layer-primo/playground/app/pages/example/chart.vue
+++ packages/nuxt-layer-primo/playground/app/pages/example/chart.vue
@@ -13,8 +13,8 @@ function setChartData() {
       {
         label: 'Sales',
         data: [540, 325, 702, 620],
-        // backgroundColor: ['rgba(249, 115, 22, 0.2)', 'rgba(6, 182, 212, 0.2)', 'rgb(107, 114, 128, 0.2)', 'rgba(139, 92, 246, 0.2)'],
-        // borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+        backgroundColor: ['rgba(249, 115, 22, 0.2)', 'rgba(6, 182, 212, 0.2)', 'rgb(107, 114, 128, 0.2)', 'rgba(139, 92, 246, 0.2)'],
+        borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
         borderWidth: 1,
       },
     ],
@@ -30,7 +30,7 @@ function setChartData() {
       :type="i "
       :data="chartData"
       :options="chartOptions"
-      class="col-span-12 h-80 2xl:col-span-4 xl:col-span-6"
+      class="col-span-12 h-80 md:col-span-6 xl:col-span-4"
     />
   </div>
 </template>
