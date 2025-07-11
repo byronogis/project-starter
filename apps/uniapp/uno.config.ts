861865aa6899924f235949d23362398060b03837
byronogis <byronogis@outlook.com>
2025-07-11 10:27:36 +0800

    feat(unocss): 支持图标部分区域显示原始色彩

--- apps/uniapp/uno.config.ts
+++ apps/uniapp/uno.config.ts
@@ -28,6 +28,17 @@ export default base(
         // HBuilderX 必须针对要使用的 Collections 做异步导入
         // carbon: () => import('@iconify-json/carbon/icons.json').then(i => i.default),
       },
+      // 针对需要固定保留部分区域色彩的情况, 叠加一个仅包含色彩区域的背景图, 配合底层的背景色,
+      // 使得仅 currentColor 的部分会自动变色, 而非 currentColor 的部分则固定保持不变
+      processor(css, { mode, collection, name }) {
+        if (mode === 'mask' && collection === 'custom' && name.includes('reserved')) {
+          delete css['background-color']
+          const reservedSvg = (css['--un-icon'] as string).replaceAll('currentColor', 'transparent')
+
+          css.background = `${reservedSvg} no-repeat, currentColor`
+          css['background-size'] = '100% 100%'
+        }
+      },
     },
   },
   {
