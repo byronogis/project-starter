b757276373377596b206d84d8159ccbb0e111cf6
byronogis <byronogis@outlook.com>
2025-07-17 16:24:24 +0800

    feat(apps/uniapp): 添加 _directResponse 选项, 可选是否直接返回 data 字段

--- apps/uniapp/src/api/index.ts
+++ apps/uniapp/src/api/index.ts
@@ -1,24 +1,63 @@
 import type { UnConfig, UnResponse } from '@uni-helper/uni-network'
 import { un } from '@uni-helper/uni-network'
 
-export const http = un.create({
+const _http = un.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  _directResponse: true, // 默认直接返回响应数据
 })
 
-http.interceptors.response.use((response: UnResponse) => {
-  const _data = response.data as BaseResult
+_http.interceptors.response.use((response: UnResponse) => {
+  const _data = response.data as BaseResult | undefined
   const _skipCheck = response.config?._skipResponseCheck
-  if (!_skipCheck && _data.code !== 200) {
-    return Promise.reject(new Error(_data.message ?? `[HTTP] Unknown Error: ${{ response }}`))
+  if (!_skipCheck && _data?.code !== 200) {
+    return Promise.reject(new Error(_data?.message ?? `[HTTP] Unknown Error: ${{ response }}`))
   }
 
-  return Promise.resolve(response)
+  const _directResponse = response.config?._directResponse
+
+  return Promise.resolve(_directResponse ? (_data as any) : response)
 })
 
-export interface BaseResult {
+const originalRequest = _http.request.bind(_http) // 保存原始方法
+
+/**
+ * 自定义请求方法, 对于配置 _directResponse 区分不同的返回类型
+ *
+ * 当 _directResponse 为 false 时，完整返回 \
+ * 当 _directResponse 为 true 或未设置时，直接返回接口数据 \
+ * 如果不传 _directResponse，取默认值 true \
+ */
+function customRequest<Result = any, Params = any>(
+  configOrUrl: string | RequestConfig<Result, Params> & { _directResponse: false },
+  config?: RequestConfig<Result, Params> & { _directResponse: false }
+): Promise<UnResponse<Result>>
+function customRequest<Result = any, Params = any>(
+  configOrUrl: string | RequestConfig<Result, Params> & { _directResponse?: true },
+  config?: RequestConfig<Result, Params> & { _directResponse?: true }
+): Promise<Result>
+function customRequest<Result = any, Params = any>(
+  configOrUrl: string | RequestConfig<Result, Params>,
+  config?: RequestConfig<Result, Params>
+): Promise<Result>
+function customRequest<Result = any, Params = any>(
+  configOrUrl: string | RequestConfig<Result, Params>,
+  config?: RequestConfig<Result, Params>,
+): Promise<Result | UnResponse<Result>> {
+  return originalRequest<Result, Params, UnResponse<Result>>(configOrUrl, config) as any
+}
+
+/**
+ * 创建新的 http 对象, 使得 request 的类型得到正确推断
+ */
+export const http = {
+  ..._http,
+  request: customRequest, // 替换原有的 request 方法
+}
+
+export interface BaseResult<D = any> {
   code: number
   message?: string
-  data?: any
+  data: D
 }
 
 interface ExtraRequestConfig {
@@ -26,6 +65,11 @@ interface ExtraRequestConfig {
    * 跳过响应检查
    */
   _skipResponseCheck?: boolean
+  /**
+   * 直接返回响应数据, 会直接取 data 字段作为结果
+   * @default true
+   */
+  _directResponse?: boolean
 }
 
 export type RequestConfig<R = any, T = any> = UnConfig<R, T> & ExtraRequestConfig
@@ -52,8 +96,6 @@ interface ExampleData {
   id: string
 }
 
-interface ExampleResult extends BaseResult {
-  data: {
-    list: string[]
-  }
-}
+type ExampleResult = BaseResult<{
+  list: string[]
+}>
