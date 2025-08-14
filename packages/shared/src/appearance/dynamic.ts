2f7149e5361bf518f2471c8b5c38d5613cb23b91
byronogis <byronogis@outlook.com>
2025-08-14 17:03:46 +0800

    feat(shared/apperance): add style by need

--- packages/shared/src/appearance/dynamic.ts
+++ packages/shared/src/appearance/dynamic.ts
@@ -19,6 +19,34 @@ export function dynamic(event: MouseEvent, options: SharedAppearanceOptions) {
     return
   }
 
+  /**
+   * Add styles to the head if not already present.
+   */
+  if (!document.querySelector('style[data-hid="appearance-dynamic"]')) {
+    const style = document.createElement('style')
+    style.setAttribute('data-hid', 'appearance-dynamic')
+    style.textContent = `
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+::view-transition-old(root) {
+  z-index: 1;
+}
+::view-transition-new(root) {
+  z-index: 2147483646;
+}
+.dark::view-transition-old(root) {
+  z-index: 2147483646;
+}
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+`
+    document.head.appendChild(style)
+  }
+
   const x = event.clientX
   const y = event.clientY
   const endRadius = Math.hypot(
@@ -50,24 +78,3 @@ export function dynamic(event: MouseEvent, options: SharedAppearanceOptions) {
       )
     })
 }
-
-// Add those styles to your CSS
-/*
-::view-transition-old(root),
-::view-transition-new(root) {
-  animation: none;
-  mix-blend-mode: normal;
-}
-::view-transition-old(root) {
-  z-index: 1;
-}
-::view-transition-new(root) {
-  z-index: 2147483646;
-}
-.dark::view-transition-old(root) {
-  z-index: 2147483646;
-}
-.dark::view-transition-new(root) {
-  z-index: 1;
-}
-*/
